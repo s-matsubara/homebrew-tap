18c5dbcda0640e7fe508cf8001539ab409dd9f3d
Suguru Matsubara <porto.messias@icloud.com>
2023-08-23 23:17:29 +0900

    Update cloud-cli version and dependencies

--- Formula/cloud-cli.rb
+++ Formula/cloud-cli.rb
@@ -1,22 +1,23 @@
 class CloudCli < Formula
   url "https://github.com/s-matsubara/cloud-cli.git",
-      tag: "0.1.0"
+      tag: "0.1.1"
   desc ""
   homepage "https://github.com/s-matsubara/"
 
   depends_on "fzf"
-  depends_on "google-cloud-sdk"
   depends_on "azure-cli"
   depends_on "kubectl"
 
   def install
-    opt_prefix.install "cloud-cli.zsh", "gcp.zsh", "azure.zsh", "k8s.zsh"
+    pkgshare.install "cloud-cli.zsh", "gcp.zsh", "azure.zsh", "aws.zsh", "k8s.zsh"
   end
 
   def caveats
     <<~EOS
       Add the following line to your ~/.zshrc file:
-        [[ -f $(brew --prefix)/opt/cloud-cli/cloud-cli.zsh  ]] && . $(brew --prefix)/opt/cloud-cli/cloud-cli.zsh
+
+        [[ -f $(brew --prefix)/share/cloud-cli/cloud-cli.zsh  ]] && . $(brew --prefix)/share/cloud-cli/cloud-cli.zsh
+
     EOS
   end
 end
