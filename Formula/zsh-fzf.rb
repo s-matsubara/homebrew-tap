5039a75a69eda7d6951bb463f57090760411406b
Suguru Matsubara <porto.messias@icloud.com>
2023-08-27 09:12:58 +0900

    Replace cloud-cli formula with updated zsh-fzf formula

---
+++ Formula/zsh-fzf.rb
@@ -0,0 +1,23 @@
+class CloudCli < Formula
+  url "https://github.com/s-matsubara/zsh-fzf.git",
+      tag: "0.2.1"
+  desc ""
+  homepage "https://github.com/s-matsubara/"
+
+  depends_on "fzf"
+  depends_on "azure-cli"
+  depends_on "kubectl"
+
+  def install
+    pkgshare.install "zsh-fzf.zsh", Dir["cloud-cli", "k8s"]
+  end
+
+  def caveats
+    <<~EOS
+      Add the following line to your ~/.zshrc file:
+
+        [[ -f $(brew --prefix)/share/zsh-fzf/zsh-fzf.zsh  ]] && . $(brew --prefix)/share/zsh-fzf/zsh-fzf.zsh
+
+    EOS
+  end
+end
